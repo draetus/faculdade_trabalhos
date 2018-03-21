b5f85edda5e0e40d40329efebb0f8817536fb8b2
Mauricio Macario de Farias Junior <6311130@univali.br>
2018-03-20 22:00:34 -0300

    Added Exercicio 1 POO2

---
+++ POO2/Exercicio_1.py
@@ -0,0 +1,27 @@
+class Product(object):
+    __name = ""
+    __description = ""
+    managementFee = 0.0
+
+    def __init__(self, name):
+        self.__name = name
+        print(name)
+        print("Product")
+
+class Loan(Product):
+    __interest = 0.0
+    __lenders = []
+
+class ManagedFund(Product):
+    __fundId = ""
+    __distribution= 0.0
+    __currentUnitPrice = 0.0
+
+class Cash(Product):
+    __interest = 0.0
+
+    def __init__(self):
+        super(Cash, self).__init__("Jorge")
+        print("Cash")
+
+a = Cash()
